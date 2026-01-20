e6a97a2df697cf8face41ccef594e4924bb97b6f
Andrew Ash <andrew@emotiveapps.com>
2026-01-20 02:22:18 -0500

    Added template .gitignore for Swift projects

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "MimeTypeEnum",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "MimeTypeEnum",
+            targets: ["MimeTypeEnum"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "MimeTypeEnum"
+        ),
+        .testTarget(
+            name: "MimeTypeEnumTests",
+            dependencies: ["MimeTypeEnum"]
+        ),
+    ]
+)
